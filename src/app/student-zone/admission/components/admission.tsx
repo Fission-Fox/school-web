b782a32e1ee50d7fc3d2dd2c0b00a32ba5e22185
mubashir-786 <mubashirrazaimran@gmail.com>
2024-01-10 16:39:55 +0500

    fix: form added

--- src/app/student-zone/admission/components/admission.tsx
+++ src/app/student-zone/admission/components/admission.tsx
@@ -1,6 +1,7 @@
+"use client";
+import { Checkbox, FormControlLabel } from "@mui/material";
+import SelectDropdown from "./dropdown";
 // import { PhotoIcon, UserCircleIcon } from '@heroicons/react/24/solid'
-
-import Dropdown from "./dropdown";
 
 export default function Admission() {
   const admissionfor = [
@@ -31,97 +32,19 @@ export default function Admission() {
   ];
   const people3 = [{ name: "MCAT" }, { name: "BCAT" }, { name: "T ECAT" }];
   return (
-    <div className="w-[60%] m-auto space-y-10 divide-y mt-32 divide-gray-900/10">
-      <div className="grid grid-cols-1 gap-x-8 gap-y-8 md:grid-cols-3">
+    <div className="w-[50%] m-auto space-y-10 divide-y mt-32 divide-gray-900/10">
+      <div>
         <div className="px-4 sm:px-0">
-          <h2 className="text-base font-semibold leading-7 text-gray-900">
+          <h2 className="text-5xl text-center my-4 font-semibold  text-gray-900">
             ADMISSION FORM
           </h2>
-          <p className="mt-1 text-sm leading-6 text-gray-600">
-            This information will be displayed publicly so be careful what you
-            share.
-          </p>
         </div>
 
-        <form className="bg-white shadow-sm ring-1 ring-gray-900/5 sm:rounded-xl md:col-span-2">
-          {/* <div className="px-4 py-6 sm:p-8">
-                        <div className="grid max-w-2xl grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
-                            <div className="sm:col-span-4">
-                                <label htmlFor="website" className="block text-sm font-medium leading-6 text-gray-900">
-                                    Website
-                                </label>
-                                <div className="mt-2">
-                                    <div className="flex rounded-md shadow-sm ring-1 ring-inset ring-gray-300 focus-within:ring-2 focus-within:ring-inset focus-within:ring-indigo-600 sm:max-w-md">
-                                        <span className="flex select-none items-center pl-3 text-gray-500 sm:text-sm">http://</span>
-                                        <input
-                                            type="text"
-                                            name="website"
-                                            id="website"
-                                            className="block flex-1 border-0 bg-transparent py-1.5 pl-1 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6"
-                                            placeholder="www.example.com"
-                                        />
-                                    </div>
-                                </div>
-                            </div>
-
-                            <div className="col-span-full">
-                                <label htmlFor="about" className="block text-sm font-medium leading-6 text-gray-900">
-                                    About
-                                </label>
-                                <div className="mt-2">
-                                    <textarea
-                                        id="about"
-                                        name="about"
-                                        rows={3}
-                                        className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-                                        defaultValue={''}
-                                    />
-                                </div>
-                                <p className="mt-3 text-sm leading-6 text-gray-600">Write a few sentences about yourself.</p>
-                            </div>
-
-                            <div className="col-span-full">
-                                <label htmlFor="photo" className="block text-sm font-medium leading-6 text-gray-900">
-                                    Photo
-                                </label>
-                                <div className="mt-2 flex items-center gap-x-3">
-
-                                    <button
-                                        type="button"
-                                        className="rounded-md bg-white px-2.5 py-1.5 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50"
-                                    >
-                                        Change
-                                    </button>
-                                </div>
-                            </div>
-
-                            <div className="col-span-full">
-                                <label htmlFor="cover-photo" className="block text-sm font-medium leading-6 text-gray-900">
-                                    Cover photo
-                                </label>
-                                <div className="mt-2 flex justify-center rounded-lg border border-dashed border-gray-900/25 px-6 py-10">
-                                    <div className="text-center">
-
-                                        <div className="mt-4 flex text-sm leading-6 text-gray-600">
-                                            <label
-                                                htmlFor="file-upload"
-                                                className="relative cursor-pointer rounded-md bg-white font-semibold text-indigo-600 focus-within:outline-none focus-within:ring-2 focus-within:ring-indigo-600 focus-within:ring-offset-2 hover:text-indigo-500"
-                                            >
-                                                <span>Upload a file</span>
-                                                <input id="file-upload" name="file-upload" type="file" className="sr-only" />
-                                            </label>
-                                            <p className="pl-1">or drag and drop</p>
-                                        </div>
-                                        <p className="text-xs leading-5 text-gray-600">PNG, JPG, GIF up to 10MB</p>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div> */}
+        <form className="bg-white shadow-sm ring-1 py-3 ring-gray-900/5 sm:rounded-xl md:col-span-2">
           <div className="flex justify-evenly">
-            <Dropdown heading={"Admission for"} list={admissionfor} />
-            <Dropdown heading={"Admission type"} list={people} />
-            <Dropdown heading={"Classes"} list={people2} />
+            <SelectDropdown label={"Admission for"} list={admissionfor} />
+            <SelectDropdown label={"Admission type"} list={people} />
+            <SelectDropdown label={"Classes"} list={people2} />
           </div>
           {/* <div className="flex justify-center">
                         <Example heading={"Federal / AKU-EB"} list={people3} />
@@ -132,40 +55,58 @@ export default function Admission() {
               <div className="font-semibold">Science Subjects</div>
               <div className="text-[14px] pl-1 w-32 mt-2">
                 <div className="mb-1">
-                  <input type="checkbox" className="mr-1 " />
-                  BIOLOGY
+                  <FormControlLabel
+                    control={<Checkbox defaultChecked />}
+                    label="BIOLOGY"
+                  />
                 </div>
                 <div className="mb-1">
-                  <input type="checkbox" className="mr-1" />
-                  COMPUTER
+                  <FormControlLabel
+                    control={<Checkbox defaultChecked />}
+                    label="  COMPUTER"
+                  />
                 </div>
                 <div className="mb-1">
-                  <input type="checkbox" className="mr-1" />
-                  PHYSICS
+                  <FormControlLabel
+                    control={<Checkbox defaultChecked />}
+                    label="PHYSICS"
+                  />
                 </div>
                 <div className="mb-1">
-                  <input type="checkbox" className="mr-1" />
-                  CHEMISTRY
+                  <FormControlLabel
+                    control={<Checkbox defaultChecked />}
+                    label="CHEMISTRY"
+                  />
                 </div>
                 <div className="mb-1">
-                  <input type="checkbox" className="mr-1" />
-                  MATHEMATICS
+                  <FormControlLabel
+                    control={<Checkbox defaultChecked />}
+                    label="MATHEMATICS"
+                  />
                 </div>
                 <div className="mb-1">
-                  <input type="checkbox" className="mr-1" />
-                  URDU
+                  <FormControlLabel
+                    control={<Checkbox defaultChecked />}
+                    label="URDU"
+                  />
                 </div>
                 <div className="mb-1">
-                  <input type="checkbox" className="mr-1" />
-                  PAK.STUDIES
+                  <FormControlLabel
+                    control={<Checkbox defaultChecked />}
+                    label="  PAK.STUDIES"
+                  />
                 </div>
                 <div className="mb-1">
-                  <input type="checkbox" className="mr-1" />
-                  ENGLISH
+                  <FormControlLabel
+                    control={<Checkbox defaultChecked />}
+                    label="ENGLISH"
+                  />
                 </div>
                 <div className="mb-1">
-                  <input type="checkbox" className="mr-1" />
-                  ISLAMIAT
+                  <FormControlLabel
+                    control={<Checkbox defaultChecked />}
+                    label="ISLAMIAT"
+                  />
                 </div>
               </div>
             </div>
@@ -252,22 +193,9 @@ export default function Admission() {
               </div>
             </div>
           </div>
-        </form>
-      </div>
-
-      <div className="grid grid-cols-1 gap-x-8 gap-y-8 pt-10 md:grid-cols-3">
-        <div className="px-4 sm:px-0">
-          <h2 className="text-base font-semibold leading-7 text-gray-900">
-            Personal Information
-          </h2>
-          <p className="mt-1 text-sm leading-6 text-gray-600">
-            Use a permanent address where you can receive mail.
-          </p>
-        </div>
-
-        <form className="bg-white shadow-sm ring-1 ring-gray-900/5 sm:rounded-xl md:col-span-2">
+          <hr />
           <div className="px-4 py-6 sm:p-8">
-            <div className="grid max-w-2xl grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
+            <div className="grid max-w-full grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
               <div className="sm:col-span-3">
                 <label
                   htmlFor="first-name"
@@ -556,14 +484,8 @@ export default function Admission() {
           </div>
           <div className="flex items-center justify-center gap-x-6 border-t border-gray-900/10 px-4 py-4 sm:px-8">
             <button
-              type="button"
-              className="text-sm font-semibold leading-6 text-gray-900"
-            >
-              Cancel
-            </button>
-            <button
               type="submit"
-              className="rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+              className="rounded-md text-xl bg-indigo-600 px-8 py-4 font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
             >
               Save
             </button>
